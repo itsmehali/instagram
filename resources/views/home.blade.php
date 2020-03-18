eb6739bcf2b4b2aa1189e4780b15491d52b39025
vagrant <vagrant@homestead>
2020-03-18 15:25:37 +0000

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://scontent-vie1-1.cdninstagram.com/v/t51.2885-19/s320x320/80714293_1744829738975439_9138857800707866624_n.jpg?_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_ohc=vU0hfmimcC0AX-QVRaG&oh=e163592bcd2805309d25c4992d259ec1&oe=5EA3C65C" class="rounded-circle" style="max-height: 175px;">
+        </div>
+        <div class="col-9">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>We are very good at playing World of warcraft and i love cookies</div>
+            <div><a href="">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://scontent-vie1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/88374782_543733416501650_6638052850750298038_n.jpg?_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=CnsfGm2y3SMAX-jVn-n&oh=018a4ddecbea9d72fc973c67a5e62d9a&oe=5E9ABE49" class="w-100">
+        </div>
+
+        <div class="col-4">
+            <img src="https://scontent-vie1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/88374782_543733416501650_6638052850750298038_n.jpg?_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=CnsfGm2y3SMAX-jVn-n&oh=018a4ddecbea9d72fc973c67a5e62d9a&oe=5E9ABE49" class="w-100">
+        </div>
+
+        <div class="col-4">
+            <img src="https://scontent-vie1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/88374782_543733416501650_6638052850750298038_n.jpg?_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=CnsfGm2y3SMAX-jVn-n&oh=018a4ddecbea9d72fc973c67a5e62d9a&oe=5E9ABE49" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
