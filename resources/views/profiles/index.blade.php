9a6b6e38eaf191246539cc04fbd59e4a23691095
vagrant <vagrant@homestead>
2020-03-20 13:47:45 +0000

    -

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,15 +7,18 @@
             <img src="https://scontent-vie1-1.cdninstagram.com/v/t51.2885-19/s320x320/80714293_1744829738975439_9138857800707866624_n.jpg?_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_ohc=vU0hfmimcC0AX-QVRaG&oh=e163592bcd2805309d25c4992d259ec1&oe=5EA3C65C" class="rounded-circle" style="max-height: 175px;">
         </div>
         <div class="col-9">
-            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="">Add New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-            <div>We are very good at playing World of warcraft and i love cookies</div>
-            <div><a href="">www.freecodecamp.org</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="">{{ $user->profile->url }}</a></div>
         </div>
     </div>
     <div class="row pt-5">
