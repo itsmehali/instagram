7cd56b07954a6176f59dc720d2365eca1e359853
vagrant <vagrant@homestead>
2020-03-27 13:20:16 +0000

    following button and unfollow button with vue. also writing out the followers and following on the profile. + using laravel telescope

--- app/Profile.php
+++ app/Profile.php
@@ -14,6 +14,11 @@ class Profile extends Model
         return ($this->image) ? '/storage/' . $this->image : '/storage/profile/OTzOnDin4IVKHW3riyklOSegkeodHh97M4dpH0Uw.png';
     }
 
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
